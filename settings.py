24f76de460ddc6548f560d8f7364c6b8bdd68b42
Ramesh Hosamani <rhosmani@akamai.com>
2018-05-02 20:07:29 +0530

    Added domain

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g2_=ea-_z7!j=yi^%16680*67ooyi*$+i!0a*hzqwy$%w(+rm7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rameshho.pythonanywhere.com']
 
 
 # Application definition
