22b5cdb78e91d2e51b7b19071554cd75cb4a4d83
Ramesh Hosamani <rhosmani@akamai.com>
2018-05-02 13:23:36 +0530

    Removed static files

--- SugureshwaraFertilizerShop/settings.py
+++ SugureshwaraFertilizerShop/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/fertilizers/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
